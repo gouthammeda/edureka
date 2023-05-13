d22605322b60ba14214b7209487a10b930e180cc
gouthammeda <gowthambhanu111@gmail.com>
2023-05-13 10:06:57 +0530

    adding spark dataframe inputs

--- src/main/scala/SparkDataframe/userdefinedfunc.scala
+++ src/main/scala/SparkDataframe/userdefinedfunc.scala
@@ -11,13 +11,19 @@ object userdefinedfunc {
       .appName("dataframe")
       .master("local")
       .getOrCreate()
-
     spark.sparkContext.setLogLevel("ERROR")
+
     val temp = spark.read.json(args(0))
     val CtoF = udf((degCel: Double) => (degCel * 9.0 / 5.0) + 32)
-    temp.withColumn("avgHighF", CtoF(col("avgHigh"))).withColumn("avgLowF", CtoF(col("avgLow"))).show()
-    temp.withColumn("avgHighF", round(CtoF(col("avgHigh")), 1)).withColumn("avgLowF", round(CtoF(col("avgLow")), 1)).drop("avgHigh", "avgLow").show
+
+    temp.withColumn("avgHighF", CtoF(col("avgHigh")))
+      .withColumn("avgLowF", CtoF(col("avgLow")))
+      .show()
     temp.withColumn("avgHighF", round(CtoF(col("avgHigh")), 1))
+      .withColumn("avgLowF", round(CtoF(col("avgLow")), 1))
+      .drop("avgHigh", "avgLow")
+      .show
+    //temp.withColumn("avgHighF", round(CtoF(col("avgHigh")), 1))
     //round (((8.7 * 9.0 / 5.0) + 32), 1)
 
     val df = spark.read.json(args(1))
--- src/main/scala/SparkDataset/DatasetcreationJsonInput.scala
+++ src/main/scala/SparkDataset/DatasetcreationJsonInput.scala
@@ -1,11 +1,10 @@
 package SparkDataset
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
 
 
 /* Created by gouthamkumarreddymeda on 4/16/23 */
-object Dataset {
+object DatasetcreationJsonInput {
   def main(args: Array[String]) {
 
     val spark = SparkSession
@@ -14,20 +13,20 @@ object Dataset {
       .master("local")
       .getOrCreate()
     import spark.implicits._
-
-    //Dataset
-    val employeeDS = spark.read.json(args(0)).as[Employee]
-    employeeDS.show()
-    //Dataframe
-    val employeeDF = spark.read.json(args(0))
-    employeeDF.show()
+    spark.sparkContext.setLogLevel("ERROR")
 
 //    dataframe we get compile errors but in dataset we get runtime errors-- which is true
-//    in dataset we get compile time errors -- check this statement.
+//    using Seq() collection to create a dataset.
     val caseClassDS = Seq(Employee("Andy", 32)).toDS()
     caseClassDS.show
 
+    //dataset creation
     val primitiveDS = Seq(1, 2, 3).toDS()
-    primitiveDS.map(_ + 1).collect
+    primitiveDS.map(_ + 1).collect.foreach(println)
+
+    //converting dataframe to Dataset by reading a file.
+    val employeeDS = spark.read.json(args(0)).as[Employee]
+    employeeDS.show()
+
   }
 }
